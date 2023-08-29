7128484b62434709e7e6bd2b2d7221ec4c99feb2
Kashif Ullah <kashifnymph@gmail.com>
2023-08-30 00:43:04 +0500

    added server side data fetching

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,11 +1,60 @@
-import React from 'react'
-
-const BlogPost = () => {
-  return (
-    <div>
- BlogPost page
-    </div>
-  )
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+import { Avatar } from "@mui/material";
+import {notFound} from "next/navigation";
+async function getData(id) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`,{
+    next:{revalidate:10},
+  });
+  if (!res.ok) {
+    // throw new Error("Failed to fetch data");
+    throw notFound();
+  }
+  return res.json();
 }
+const BlogPost = async ({params}) => {
+  const data  = await getData(params.id)
+  console.log(data);
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.topLeft}>
+          <h1 className={styles.title}>{data?.title}</h1>
+          <p className={styles.desc}>{data?.body}</p>
+          <div className={styles.user}>
+          <Avatar className={styles.icon} alt="kashif ullah" src="/me.jpeg" />
+            <p className={styles.username}>Kashif Ullah</p>
+          </div>
+        </div>
 
-export default BlogPost
+        <div className={styles.imageContainer}>
+          <Image src="/post.svg" fill={true} alt="" className={styles.img} />
+        </div>
+      </div>
+      <div className={styles.bottom}>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos,
+          totam consequatur ipsa repudiandae dicta quis facilis, animi,
+          molestiae mollitia voluptas minus. Modi enim ea voluptatibus veniam
+          dolore sapiente earum dicta. <br />
+          <br /> Eum, architecto? Natus eligendi atque quisquam distinctio
+          maiores dicta in reprehenderit, ab, vero fuga sit et similique
+          consequatur quidem, eveniet voluptatum non labore. At, deserunt!
+          Assumenda facilis autem accusamus quae. Sequi molestias vel eaque unde
+          deserunt odit rem consequuntur saepe neque, hic nisi culpa, in
+          voluptates? Officiis itaque vero, iste illo voluptatem, quam quas,
+          earum eius repudiandae natus facilis nulla! Eum, architecto? Natus
+          eligendi atque quisquam distinctio maiores dicta in reprehenderit, ab,
+          vero fuga sit et similique consequatur quidem, eveniet voluptatum non
+          labore. At, deserunt! Assumenda facilis autem accusamus quae. Sequi
+          molestias vel eaque unde deserunt odit rem consequuntur saepe neque,
+          hic nisi culpa, in voluptates? Officiis itaque vero, iste illo
+          voluptatem, quam quas, earum eius repudiandae natus facilis nulla!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default BlogPost;
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,76 +1,45 @@
-'use client'
+"use client";
 import React, { useContext } from "react";
 import styles from "./page.module.css";
 import Link from "next/link";
 import Image from "next/image";
 import { ThemeContext } from "@/context/ThemeContext";
-const Blog = () => {
-  const {mode} = useContext(ThemeContext)
+import {notFound} from "next/navigation";
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts",{
+    next:{revalidate:10},
+  });
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
+
+const Blog = async () => {
+  const { mode } = useContext(ThemeContext);
+  const data = await getData();
   return (
     <div>
-      <Link href={"/blog/testId"} className={styles.container}>
-        <div className={styles.imageContainer}>
-          <Image
-            className={styles.img}
-            height={300}
-            width={300}
-            src={"/5.svg"}
-          />
-        </div>
-        <div className={styles.item}>
-          <div className={styles.content}>
-            <h1 className={styles.title}>Test</h1>
-            <p className={mode==='light'?styles.desklight:styles.desc}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
-              temporibus commodi, cum at numquam delectus. Commodi officiis,
-              voluptatem consectetur velit, reiciendis qui iusto quia impedit
-              ducimus vero, doloribus saepe ut.
-            </p>
+      {data?.map((item, id) => (
+        <Link href={"/blog/testId"} className={styles.container} key={id}>
+          <div className={styles.imageContainer}>
+            <Image
+              className={styles.img}
+              height={300}
+              width={300}
+              src={"/5.svg"}
+            />
           </div>
-        </div>
-      </Link>
-      <Link href={"/blog/testId"} className={styles.container}>
-        <div className={styles.imageContainer}>
-          <Image
-            className={styles.img}
-            height={300}
-            width={300}
-            src={"/6.svg"}
-          />
-        </div>
-        <div className={styles.item}>
-          <div className={styles.content}>
-            <h1 className={styles.title}>Test</h1>
-            <p className={mode==='light'?styles.desklight:styles.desc}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
-              temporibus commodi, cum at numquam delectus. Commodi officiis,
-              voluptatem consectetur velit, reiciendis qui iusto quia impedit
-              ducimus vero, doloribus saepe ut.
-            </p>
+          <div className={styles.item}>
+            <div className={styles.content}>
+              <h1 className={styles.title}>{item?.title}</h1>
+              <p className={mode === "light" ? styles.desklight : styles.desc}>
+                {item?.body}
+              </p>
+            </div>
           </div>
-        </div>
-      </Link>
-      <Link href={"/blog/testId"} className={styles.container}>
-        <div className={styles.imageContainer}>
-          <Image
-            className={styles.img}
-            height={300}
-            width={300}
-            src={"/7.svg"}
-          />
-        </div>
-        <div className={styles.item}>
-          <div className={styles.content}>
-            <h1 className={styles.title}>Test</h1>
-            <p className={mode==='light'?styles.desklight:styles.desc}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
-              temporibus commodi, cum at numquam delectus. Commodi officiis,
-              voluptatem consectetur velit, reiciendis qui iusto quia impedit
-              ducimus vero, doloribus saepe ut.
-            </p>
-          </div>
-        </div>
-      </Link>
+        </Link>
+      ))}
     </div>
   );
 };
